ef83ff90ec7f78c932c37fd92da860e8387f6014
Maxim Sharabayko <maxlovic@gmail.com>
2021-02-28 12:18:42 +0100

    Added drift tracer

---
+++ app/tsbpd.cpp
@@ -0,0 +1,62 @@
+#include "tsbpd.hpp"
+
+using namespace std::chrono;
+
+static const uint32_t MAX_TIMESTAMP = 0xFFFFFFFF; //Full 32 bit (01h11m35s)
+
+steady_clock::time_point tsbpd::get_time_base(uint32_t timestamp_us)
+{
+    /*
+     * Packet timestamps wrap around every 01h11m35s (32-bit in usec)
+     * When added to the peer start time (base time),
+     * wrapped around timestamps don't provide a valid local packet delevery time.
+     *
+     * A wrap check period starts 30 seconds before the wrap point.
+     * In this period, timestamps smaller than 30 seconds are considered to have wrapped around (then adjusted).
+     * The wrap check period ends 30 seconds after the wrap point, afterwhich time base has been adjusted.
+     */
+    int64_t carryover = 0;
+
+    // This function should generally return the timebase for the given timestamp_us.
+    // It's assumed that the timestamp_us, for which this function is being called,
+    // is received as monotonic clock. This function then traces the changes in the
+    // timestamps passed as argument and catches the moment when the 64-bit timebase
+    // should be increased by a "segment length" (MAX_TIMESTAMP+1).
+
+    // The checks will be provided for the following split:
+    // [INITIAL30][FOLLOWING30]....[LAST30] <-- == CPacket::MAX_TIMESTAMP
+    //
+    // The following actions should be taken:
+    // 1. Check if this is [LAST30]. If so, ENTER TSBPD-wrap-check state
+    // 2. Then, it should turn into [INITIAL30] at some point. If so, use carryover MAX+1.
+    // 3. Then it should switch to [FOLLOWING30]. If this is detected,
+    //    - EXIT TSBPD-wrap-check state
+    //    - save the carryover as the current time base.
+
+    if (m_bTsbPdWrapCheck)
+    {
+        // Wrap check period.
+
+        if (timestamp_us < TSBPD_WRAP_PERIOD)
+        {
+            carryover = int64_t(MAX_TIMESTAMP) + 1;
+        }
+        //
+        else if ((timestamp_us >= TSBPD_WRAP_PERIOD) && (timestamp_us <= (TSBPD_WRAP_PERIOD * 2)))
+        {
+            /* Exiting wrap check period (if for packet delivery head) */
+            m_bTsbPdWrapCheck = false;
+            m_tsTsbPdTimeBase += microseconds_from(int64_t(MAX_TIMESTAMP) + 1);
+            spdlog::info("TSBPD wrap period ends with ts={}, drift: {}", timestamp_us, m_drift_tracer.drift());
+        }
+    }
+    // Check if timestamp_us is in the last 30 seconds before reaching the MAX_TIMESTAMP.
+    else if (timestamp_us > (MAX_TIMESTAMP - TSBPD_WRAP_PERIOD))
+    {
+        /* Approching wrap around point, start wrap check period (if for packet delivery head) */
+        m_bTsbPdWrapCheck = true;
+        spdlog::info("TSBPD wrap period begins with ts={}, drift: {}", timestamp_us, m_drift_tracer.drift());
+    }
+
+    return (m_tsTsbPdTimeBase + microseconds_from(carryover));
+}
--- app/utils.hpp
+++ app/utils.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "stdafx.hpp"
 
 /// Running moving average (RMA).
@@ -13,4 +14,39 @@ template <std::size_t N, typename ValueType>
 inline ValueType avg_rma_w(ValueType old_value, ValueType new_value, std::size_t new_val_weight)
 {
     return (old_value * (N - new_val_weight) + new_value * new_val_weight) / N;
+}
+
+inline long long count_microseconds(const std::chrono::steady_clock::duration &t)
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(t).count();
+}
+
+inline long long count_microseconds(const std::chrono::steady_clock::time_point tp)
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(tp.time_since_epoch()).count();
+}
+
+inline long long count_milliseconds(const std::chrono::steady_clock::duration &t)
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(t).count();
+}
+
+inline long long count_seconds(const std::chrono::steady_clock::duration &t)
+{
+    return std::chrono::duration_cast<std::chrono::seconds>(t).count();
+}
+
+inline std::chrono::steady_clock::duration microseconds_from(int64_t t_us)
+{
+    return std::chrono::microseconds(t_us);
+}
+
+inline std::chrono::steady_clock::duration milliseconds_from(int64_t t_ms)
+{
+    return std::chrono::milliseconds(t_ms);
+}
+
+inline std::chrono::steady_clock::duration seconds_from(int64_t t_s)
+{
+    return std::chrono::seconds(t_s);
 }
